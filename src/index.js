9a93998d02088dbf9de797d69402112ef19567f4
RomanRomanov998 <romanovramik@gmail.com>
2025-10-17 20:17:57 +0200

    Initial commit: Webpack setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import './index.css';
+import jsLogo from './assets/javascript.jpg'; 
+
+const h1 = document.createElement('h1');
+h1.textContent = 'I love JavaScript';
+
+const img = document.createElement('img');
+img.src = jsLogo;
+
+document.body.append(h1, img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,51 @@
-import path from 'path';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-export default {
+module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve('./dist'),
-    publicPath: '',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    publicPath: '/',
   },
-  mode: 'development'
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js|mjs|cjs)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
 };
